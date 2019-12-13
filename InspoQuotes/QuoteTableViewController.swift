fef3413fc221692f5c1a4aa6904990b62de73630
Tim Newton <tim.newton@eduserve.us>
2019-12-12 16:50:24 -0800

    Lesson 273 complete. Project complete.

--- InspoQuotes/QuoteTableViewController.swift
+++ InspoQuotes/QuoteTableViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import StoreKit
 
 class QuoteTableViewController: UITableViewController {
+    static let productID = "us.eduserve.InspoQuotes.PremiumQuotes"
+    
+    var hasPurchasedPremiumQuotes:Bool {
+        return UserDefaults.standard.bool(forKey: QuoteTableViewController.productID)
+    }
     
     var quotesToShow = [
         "Our greatest glory is not in never falling, but in rising every time we fall. — Confucius",
@@ -27,27 +33,118 @@ class QuoteTableViewController: UITableViewController {
         "Your true success in life begins only when you make the commitment to become excellent at what you do. — Brian Tracy",
         "Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got to keep going. – Chantal Sutherland"
     ]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        showPremiumQuotes()
+        
+        SKPaymentQueue.default().add(self)
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return quotesToShow.count
+        return quotesToShow.count + (hasPurchasedPremiumQuotes ? 0 : 1)
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "QuoteCell", for: indexPath)
-
-        cell.textLabel?.text = quotesToShow[indexPath.row]
-
+        
+        cell.textLabel?.numberOfLines = 0
+        
+        if indexPath.row == quotesToShow.count {
+            cell.textLabel?.text = "Get More Quotes!"
+            cell.textLabel?.textColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+            cell.accessoryType = .disclosureIndicator
+        }
+        else {
+            cell.textLabel?.text = quotesToShow[indexPath.row]
+            cell.textLabel?.textColor = nil
+            cell.accessoryType = .none
+        }
+        
         return cell
     }
+    
+    // MARK: - Table view delegate source
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == quotesToShow.count {
+            buyPremiumQuotes()
+        }
         
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: - In-App Purchases
+    
+    //MARK: - IBActions
+    
     @IBAction func restorePressed(_ sender: UIBarButtonItem) {
+        SKPaymentQueue.default().restoreCompletedTransactions()
+    }
+}
+
+//MARK: - SKPaymentTransactionObserver methods
+
+extension QuoteTableViewController: SKPaymentTransactionObserver {
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+        for transaction in transactions {
+            switch transaction.transactionState {
+            case .purchasing:
+                print("Transaction in progress")
+            case .purchased:
+                print("Transaction successful")
+                unlockContent(transaction: transaction)
+            case .failed:
+                print("Transaction failed")
+                if let error = transaction.error {
+                    print("Transaction error: \(error.localizedDescription)")
+                }
+                SKPaymentQueue.default().finishTransaction(transaction)
+            case .restored:
+                print("Transaction restore request")
+                unlockContent(transaction: transaction)
+            case .deferred:
+                print("Transaction deferred")
+            @unknown default:
+                print("Transaction unknown")
+            }
+        }
+    }
+    
+    func unlockContent(transaction:SKPaymentTransaction) {
+        print("Unlocking premium content...")
+        UserDefaults.standard.set(true, forKey: QuoteTableViewController.productID)
+        navigationItem.setRightBarButton(nil, animated: true)
+        showPremiumQuotes()
         
+        // Call after unlocking content
+        SKPaymentQueue.default().finishTransaction(transaction)
+    }
+    
+    func showPremiumQuotes() {
+        if hasPurchasedPremiumQuotes {
+            print("Premium content unlocked")
+            quotesToShow.append(contentsOf: premiumQuotes)
+        }
+        else {
+            print("Premium content NOT unlocked")
+        }
+        tableView.reloadData()
+    }
+    
+    func buyPremiumQuotes() {
+        if SKPaymentQueue.canMakePayments() {
+            print("User initiated purchase...")
+            let paymentRequest = SKMutablePayment()
+            paymentRequest.productIdentifier = QuoteTableViewController.productID
+            
+            SKPaymentQueue.default().add(paymentRequest)
+        }
+        else {
+            print("User can't make payments.")
+        }
     }
 }
